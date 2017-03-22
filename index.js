ba020347bc2a1b01e4c35f6741d8c04a5fb2c2e2
Oli Lane <ojgl2@cam.ac.uk>
2017-03-22 20:00:32 +0000

    Switch to amazon's alexa sdk

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 var binScraper = require('./binTimeScraper');
+var alexa = require('alexa-app');
+
+var app = new alexa.app('binTimeScraper');
 
 binScraper.getUpcomingBinsFromUprn('200004177341').then(console.log);
