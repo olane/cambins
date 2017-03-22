3da172bfde0af9953b7d0dfdc7d89bbe1a788762
Oli Lane <ojgl2@cam.ac.uk>
2017-03-22 20:40:05 +0000

    Add some alexa logic to handle voice intents

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 'use strict';
 
 var binScraper = require('./binTimeScraper');
-var alexa = require('alexa-app');
+var voiceFormatter = require('./binTimeVoiceFormatter');
+var Alexa = require('alexa-sdk');
 
-var app = new alexa.app('binTimeScraper');
+function emitTell(content){
+	this.emit(":tell", content);
+};
 
-binScraper.getUpcomingBinsFromUprn('200004177341').then(console.log);
+var handlers = {
+	'LaunchRequest': function () {
+        this.emit('GetUpcomingBinTimes');
+    },
+    'NextBinTimeIntent': function () {
+        this.emit('GetUpcomingBinTimes');
+    },
+    'GetNextBinTime': function () {
+		binScraper.getNextBinsFromUprn('200004177341')
+			.then(voiceFormatter.formatBinEntry)
+			.then(emitTell.bind(this));
+    }
+};
+
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+  	alexa.registerHandlers(handlers);
+    alexa.execute();
+};
