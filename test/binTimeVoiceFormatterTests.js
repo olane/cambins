aeb9643d54cd13c80eb6f4341227525868e361d3
Oli Lane <ojgl2@cam.ac.uk>
2017-03-23 22:45:51 +0000

    Time based tests for voice formatter

--- test/binTimeVoiceFormatterTests.js
+++ test/binTimeVoiceFormatterTests.js
@@ -5,6 +5,7 @@ var Promise = require('promise');
 var expect = chai.expect;
 var _ = require('lodash');
 var moment = require('moment');
+var sinon = require('sinon');
 
 var voiceFormatter = require('../binTimeVoiceFormatter');
 
@@ -40,5 +41,51 @@ describe('binTimeVoiceFormatter', function() {
 
 		expect(voiceFormatter.formatBinEntry(binTime)).to.be.equal('The blue and green bins will be collected today. This is a rescheduled collection.');
 		done();
+	});
+
+	describe('time based tests', function() {
+		var clock;
+		var currentTime = new Date('2017-04-08T01:10:00'); // Saturday 8th April
+
+		before(function () {
+			clock = sinon.useFakeTimers(currentTime);
+		});
+
+		after(function () {
+			clock.restore();
+		});
+
+		it('formats a black bin collection on a day next week correctly', function(done){
+			var binTime = {
+				date: moment().add(2, 'days'),
+				binTypes: ['black'],
+				isRescheduled: false
+			};
+
+			expect(voiceFormatter.formatBinEntry(binTime)).to.be.equal('The black bins will be collected on Monday.');
+			done();
+		});
+
+		it('formats a black bin collection on a day two weeks away correctly', function(done){
+			var binTime = {
+				date: moment().add(2, 'weeks'),
+				binTypes: ['black'],
+				isRescheduled: false
+			};
+
+			expect(voiceFormatter.formatBinEntry(binTime)).to.be.equal('The black bins will be collected on Saturday 22nd April.');
+			done();
+		});
+
+		it('formats a black bin collection on a day next month correctly', function(done){
+			var binTime = {
+				date: moment().add(5, 'weeks').add(4, 'days'),
+				binTypes: ['black'],
+				isRescheduled: false
+			};
+
+			expect(voiceFormatter.formatBinEntry(binTime)).to.be.equal('The black bins will be collected on Wednesday 17th May.');
+			done();
+		});
 	});
 });
