5484168ff25178d67390cbcdb9f474e346776764
Oli Lane <ojgl2@cam.ac.uk>
2017-03-23 22:12:47 +0000

    Add a few more tests

--- test/binTimeScraperTests.js
+++ test/binTimeScraperTests.js
@@ -6,6 +6,7 @@ var expect = chai.expect;
 var sinon = require('sinon');
 var proxyquire = require('proxyquire');
 var fs = require('fs');
+var _ = require('lodash');
 
 var pageGetter = require('../pageGetter');
 
@@ -36,6 +37,59 @@ describe('binTimeScraper', function() {
 	 	binTimeScraper.getUpcomingBinsFromUprn('0000000').then(function(response){
 	 		expect(response.length).to.be.equal(24);
 	 		done();
+	 	}).catch(function(err){done(err)});
+	});
+
+	it('returns parsed bin times in time order', function(done) {
+	 	binTimeScraper.getUpcomingBinsFromUprn('0000000').then(function(response){
+	 		var lastDate;
+
+	 		_.each(response, function(x){
+	 			if(lastDate){
+	 				var assertionMessage = lastDate.format() + ' is before ' + x.date.format();
+	 				expect(lastDate.isBefore(x.date), assertionMessage).to.be.equal(true);
+	 			}
+ 				lastDate = x.date;
+	 		});
+
+	 		done();
+	 	}).catch(function(err){done(err)});
+	});
+
+	it('returns some of all bin types', function(done) {
+	 	binTimeScraper.getUpcomingBinsFromUprn('0000000').then(function(response){
+	 		var seenBinTypes = {
+	 			'blue' : false,
+	 			'green': false,
+	 			'black': false
+	 		};
+
+	 		_.each(response, function(x){
+	 			_.each(x.binTypes, function(binType){
+ 					seenBinTypes[binType] = true;
+	 			});
+	 		});
+
+	 		_.forOwn(seenBinTypes, function(value, key){
+	 			expect(value, 'Seen ' + key).to.be.equal(true);
+	 		})
+
+	 		done();
+	 	}).catch(function(err){done(err)});
+	});
+
+	it('finds at least one rescheduled time', function(done) {
+	 	binTimeScraper.getUpcomingBinsFromUprn('0000000').then(function(response){
+	 		var foundRescheduledTimes = false;
+
+	 		_.each(response, function(x){
+	 			if(x.isRescheduled){
+	 				foundRescheduledTimes = true;
+	 			}
+	 		});
+
+			expect(foundRescheduledTimes, 'found rescheduled times').to.be.equal(true);
+			done();
 	 	}).catch(function(err){done(err)});
 	});
 });
