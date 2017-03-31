f19ee0b266d5dae16e4e39a2e59a02c85a1c384d
Oli Lane <ojgl2@cam.ac.uk>
2017-03-31 18:39:28 +0100

    Fix some bugs in the alexa handling code

--- functions/cambins/index.js
+++ functions/cambins/index.js
@@ -4,16 +4,18 @@ var binScraper = require('./binTimeScraper');
 var voiceFormatter = require('./binTimeVoiceFormatter');
 var Alexa = require('alexa-sdk');
 
+const APP_ID = 'amzn1.ask.skill.6ef2339c-d9b3-4ad5-9100-1f62a4a2f98e';
+
 function emitTell(content){
 	this.emit(":tell", content);
 };
 
 var handlers = {
 	'LaunchRequest': function () {
-        this.emit('GetUpcomingBinTimes');
+        this.emit('GetNextBinTime');
     },
     'NextBinTimeIntent': function () {
-        this.emit('GetUpcomingBinTimes');
+        this.emit('GetNextBinTime');
     },
     'GetNextBinTime': function () {
 		binScraper.getNextBinsFromUprn('200004177341')
@@ -22,8 +24,9 @@ var handlers = {
     }
 };
 
-exports.handler = function(event, context, callback){
+exports.handle = function(event, context, callback){
     var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
   	alexa.registerHandlers(handlers);
     alexa.execute();
 };
