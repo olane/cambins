dca6acba76efcc31fe90a10ea1879cae89670ea1
Oli Lane <ojgl2@cam.ac.uk>
2017-04-01 12:42:41 +0100

    Add support for asking for a certain bin type

--- functions/cambins/index.js
+++ functions/cambins/index.js
@@ -3,8 +3,10 @@
 var binScraper = require('./binTimeScraper');
 var voiceFormatter = require('./binTimeVoiceFormatter');
 var Alexa = require('alexa-sdk');
+var _ = require('lodash');
 
 const APP_ID = 'amzn1.ask.skill.6ef2339c-d9b3-4ad5-9100-1f62a4a2f98e';
+const binTypes = ['green', 'blue', 'black'];
 
 function emitTell(content){
 	this.emit(":tell", content);
@@ -17,10 +19,25 @@ var handlers = {
     'NextBinTimeIntent': function () {
         this.emit('GetNextBinTime');
     },
+    'NextBinTimeForTypeIntent': function(){
+        this.emit('GetNextBinTimeForType');
+    },
     'GetNextBinTime': function () {
-		binScraper.getNextBinsFromUprn('200004177341')
-			.then(voiceFormatter.formatBinEntry)
-			.then(emitTell.bind(this));
+        binScraper.getNextBinsFromUprn('200004177341')
+            .then(voiceFormatter.formatBinEntry)
+            .then(emitTell.bind(this));
+    },
+    'GetNextBinTimeForType': function (type) {
+        var type = this.event.request.intent.slots.BinType.value;
+
+        if(!_.includes(binTypes, type)){
+            this.emit(":tell", "Sorry, I didn't recognise the bin type " + type);
+            return;
+        }
+
+        binScraper.getNextBinsFromUprnForType('200004177341', type)
+            .then(voiceFormatter.formatBinEntry)
+            .then(emitTell.bind(this));
     }
 };
 
