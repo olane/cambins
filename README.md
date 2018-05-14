b7e346f7e37863b97430819c0dac360d9950d8b9
Oli Lane <ojgl2@cam.ac.uk>
2018-05-14 18:14:00 +0100

    Convert everything to ASK v2

--- README.md
+++ README.md
@@ -5,19 +5,11 @@ WIP: An Alexa skill to find Cambridge bin collection dates
 ### To set up
 Install github.com/apex/apex
 
-`brew install awscli`
+If you haven't used the ASK CLI yet:
+`npm install -g ask-cli`
+`ask init`
 
-`aws configure`
+### To deploy:
+`cd lambda/custom && npm install && cd ../..`
 
-### To deploy to Lambda:
-`cd functions/cambins && npm install && cd ../..`
-
-`apex deploy`
-
-You need a function set up in aws lambda in one of the Alexa Skills Kit regions. It should have the name `alexa_cambins`, targetting node 6.10, with the handler `index.handle`. The trigger should be Alexa Skills Kit.
-
-### Alexa setup 
-
-Utterances, custom slots and intents are in `/alexa`
-
-Set it up to point to your lambda function's ARN.
+`ask deploy`
---
+++ lambda/custom/index.js
@@ -0,0 +1,113 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+'use strict';
+
+var binScraper = require('./binTimeScraper');
+var voiceFormatter = require('./binTimeVoiceFormatter');
+const Alexa = require('ask-sdk');
+var _ = require('lodash');
+
+const APP_ID = 'amzn1.ask.skill.6ef2339c-d9b3-4ad5-9100-1f62a4a2f98e';
+const binTypes = ['green', 'blue', 'black'];
+
+const GetNextBinTimeHandler = {
+  canHandle(handlerInput) {
+    const requestType = handlerInput.requestEnvelope.request.type;
+    const intentName = handlerInput.requestEnvelope.request.intent.name;
+
+    if(requestType === 'LaunchRequest') return true;
+
+    if (requestType === 'IntentRequest' && intentName === 'NextBinTimeIntent') {
+        return true;
+    }
+    
+    return false;
+  },
+  handle(handlerInput) {
+    return binScraper.getNextBinsFromUprn('200004177341')
+        .then(voiceFormatter.formatBinEntry)
+        .then(response => {
+          return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+        });
+  },
+};
+
+const GetNextBinTimeForTypeHandler = {
+  canHandle(handlerInput) {
+    const requestType = handlerInput.requestEnvelope.request.type;
+    const intentName = handlerInput.requestEnvelope.request.intent.name;
+
+    if(requestType === 'LaunchRequest') return true;
+
+    if (requestType === 'IntentRequest' && intentName === 'NextBinTimeIntent') {
+        return true;
+    }
+    
+    return false;
+  },
+  handle(handlerInput) {
+
+    var type = this.event.request.intent.slots.BinType.value;
+
+    if(!_.includes(binTypes, type)){
+        const response = "Sorry, I didn't recognise the bin type " + type;
+
+        return handlerInput.responseBuilder
+          .speak(speechText)
+          .getResponse();
+    }
+    
+    return binScraper.getNextBinsFromUprn('200004177341', type)
+        .then(voiceFormatter.formatBinEntry)
+        .then(response => {
+          return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+        });
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Try asking Cambridge Bins when the bins will be collected';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Try asking Cambridge Bins when the bins will be collected.')
+      .reprompt('Sorry, I can\'t understand the command. Try asking Cambridge Bins when the bins will be collected.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNextBinTimeHandler,
+    GetNextBinTimeForTypeHandler,
+    HelpIntentHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
