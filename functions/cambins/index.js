4aa091585748040ee28b0889f7395dfa68819352
Oli Lane <ojgl2@cam.ac.uk>
2018-05-14 17:34:45 +0100

    Start implementation of setting UPRNs

--- functions/cambins/index.js
+++ functions/cambins/index.js
@@ -8,11 +8,16 @@ var _ = require('lodash');
 const APP_ID = 'amzn1.ask.skill.6ef2339c-d9b3-4ad5-9100-1f62a4a2f98e';
 const binTypes = ['green', 'blue', 'black'];
 
+// Note that the normal mode is that state is empty, and that the user is just issuing one-shot requests
+var states = {
+    SETUPRN: '_SETUPRN',     // User is trying to set their UPRN.
+};
+
 function emitTell(content){
 	this.emit(":tell", content);
 };
 
-var handlers = {
+var newSessionHandlers = {
 	'LaunchRequest': function () {
         this.emit('GetNextBinTime');
     },
@@ -23,11 +28,12 @@ var handlers = {
         this.emit('GetNextBinTimeForType');
     },
     'GetNextBinTime': function () {
+        //TODO get uprn from dynamodb
         binScraper.getNextBinsFromUprn('200004177341')
             .then(voiceFormatter.formatBinEntry)
             .then(emitTell.bind(this));
     },
-    'GetNextBinTimeForType': function (type) {
+    'GetNextBinTimeForType': function () {
         var type = this.event.request.intent.slots.BinType.value;
 
         if(!_.includes(binTypes, type)){
@@ -39,14 +45,31 @@ var handlers = {
             .then(voiceFormatter.formatBinEntry)
             .then(emitTell.bind(this));
     },
+    'SetUprnIntent': function () {
+        this.handler.state = states.SETUPRN;
+        this.emit(':ask', 'What would you like to set your U.P.R.N. to? It should be a twelve digit number.');
+    },
     'Unhandled': function() {
         this.emit(':tell', 'Sorry, I didn\'t get that. Try asking Cambridge Bins when the bins will be collected.');
     }
 };
 
+var setupUprnHandlers = Alexa.CreateStateHandler(states.SETUPRN, {
+    'UprnIntent': function (){
+        //TODO set uprn in dynamodb
+    },
+    'Unhandled': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. What would you like to set your U.P.R.N. to? It should be a twelve digit number.');
+    },
+    'AMAZON.HelpIntent': function() {
+        this.emit(':ask', 'What would you like to set your U.P.R.N. to? It should be a twelve digit number. You can find it on the Cambridge bins website in the URL for your address\'s bin schedule.');
+    }
+});
+
 exports.handle = function(event, context, callback){
     var alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID;
-  	alexa.registerHandlers(handlers);
+    alexa.registerHandlers(newSessionHandlers);
+    alexa.registerHandlers(setupUprnHandlers);
     alexa.execute();
 };
