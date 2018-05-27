53e79e4c7ff1213816b873beb9b7686d6b1d90cd
Oli Lane <ojgl2@cam.ac.uk>
2018-05-27 23:18:32 +0100

    Implement setting and retrieving UPRN

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -24,14 +24,19 @@ const GetNextBinTimeHandler = {
     return false;
   },
   handle(handlerInput) {
-    return binScraper.getNextBinsFromUprn('200004177341')
-        .then(voiceFormatter.formatBinEntry)
-        .then(response => {
-          return handlerInput.responseBuilder
+    return handlerInput.attributesManager.getPersistentAttributes()
+      .then(attributes => {
+        //TODO check uprn has been set
+        return binScraper.getNextBinsFromUprn(attributes.uprn);
+        //return binScraper.getNextBinsFromUprn('200004177341');
+      })
+      .then(voiceFormatter.formatBinEntry)
+      .then(response => {
+        return handlerInput.responseBuilder
           .speak(response)
           .reprompt(response)
-            .getResponse();
-        });
+          .getResponse();
+      });
   },
 };
 
@@ -50,7 +55,7 @@ const GetNextBinTimeForTypeHandler = {
   },
   handle(handlerInput) {
     var type = handlerInput.requestEnvelope.request.intent.slots.BinType.value;
-
+    
     if(!_.includes(binTypes, type)){
         const response = "Sorry, I didn't recognise the bin type " + type;
 
@@ -59,14 +64,46 @@ const GetNextBinTimeForTypeHandler = {
           .getResponse();
     }
     
-    return binScraper.getNextBinsFromUprn('200004177341', type)
-        .then(voiceFormatter.formatBinEntry)
-        .then(response => {
-          return handlerInput.responseBuilder
+    return handlerInput.attributesManager.getPersistentAttributes()
+      .then(attributes => {
+        //TODO check uprn has been set
+        return binScraper.getNextBinsFromUprn(attributes.uprn, type);
+        //return binScraper.getNextBinsFromUprn('200004177341', type);
+      })
+      .then(voiceFormatter.formatBinEntry)
+      .then(response => {
+        return handlerInput.responseBuilder
           .speak(response)
           .reprompt(response)
-            .getResponse();
-        });
+          .getResponse();
+      });
+  },
+};
+
+const SetUprnHandler = {
+  canHandle(handlerInput) {
+    const requestType = handlerInput.requestEnvelope.request.type;
+    const intentName = handlerInput.requestEnvelope.request.intent.name;
+
+    return requestType === 'IntentRequest' && intentName === 'SetUprnIntent';
+  },
+  handle(handlerInput) {
+    if(handlerInput.requestEnvelope.request.dialogState != "COMPLETED") {
+      // let the built in dialog model reprompt for the remaining info (this will be because the UPRN was not provided)
+      return handlerInput.responseBuilder.addDelegateDirective().getResponse();
+    }
+    
+    let uprnInput = handlerInput.requestEnvelope.request.intent.slots.Uprn.value;
+    // TODO validate uprn
+    return handlerInput.attributesManager.getPersistentAttributes()
+      .then((attributes) => {
+        attributes.uprn = uprnInput;
+        handlerInput.attributesManager.setPersistentAttributes(attributes);
+        return handlerInput.attributesManager.savePersistentAttributes();
+      })
+      .then(() => {
+        return handlerInput.responseBuilder.speak('UPRN set').getResponse();
+      });
   },
 };
 
@@ -93,19 +130,22 @@ const ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Try asking Cambridge Bins when the bins will be collected.')
+      .speak('Sorry, I can\'t understand the command. Try asking Cambridge Bins when the bins will be collected. ' + error.message)
       .reprompt('Sorry, I can\'t understand the command. Try asking Cambridge Bins when the bins will be collected.')
       .getResponse();
   },
 };
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
     GetNextBinTimeHandler,
     GetNextBinTimeForTypeHandler,
+    SetUprnHandler,
     HelpIntentHandler
   )
   .addErrorHandlers(ErrorHandler)
+  .withAutoCreateTable(true)
+  .withTableName("cbins")
   .lambda();
